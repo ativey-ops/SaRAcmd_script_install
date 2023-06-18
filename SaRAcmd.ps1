e0488d3889617948647cb9e2fcf8f402d08b6f1b
ativey-ops <65632423+ativey-ops@users.noreply.github.com>
2023-06-18 10:40:57 -0400

    Added descriptive information at line 6.

--- SaRAcmd.ps1
+++ SaRAcmd.ps1
@@ -3,4 +3,5 @@ mkdir C:\Scripts\SaRAcmd
 cd C:\Scripts\SaRAcmd
 wget https://aka.ms/SaRA_EnterpriseVersionFiles -OutFile SaRAcmd.zip
 Expand-Archive -Path .\SaRAcmd.zip -DestinationPath C:\Scripts\SaRAcmd
+# The following line will uninstall Office. Different scenarios can be found at Microsoft's SaRA Enterprise Tool page. 
 .\SaRAcmd.exe -S OfficeScrubScenario -AcceptEula -OfficeVersion All
